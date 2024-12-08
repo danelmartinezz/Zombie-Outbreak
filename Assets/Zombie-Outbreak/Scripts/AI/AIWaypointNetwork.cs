8f3c114d798868b3801fbd76d41c639d2f68eba8
Danel Martinez <martinezgdanel@hotmail.com>
2024-12-07 21:53:13 -0300

    refactor: Se movió el archivo a la carpeta IA para mejor organización

---
+++ Assets/Zombie-Outbreak/Scripts/AI/AIWaypointNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PathDisplayMode { None, Connetions, Paths }
+
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    [HideInInspector]
+    public PathDisplayMode displayMode = PathDisplayMode.Connetions;
+    public Color colorLine;
+    [HideInInspector]
+    public int uiStart = 0;
+    [HideInInspector]
+    public int uiEnd = 0;
+    public List<Transform> Waypoints = new List<Transform>();
+}
