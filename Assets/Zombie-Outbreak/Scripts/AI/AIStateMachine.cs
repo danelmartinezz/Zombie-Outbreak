00b7f415288433a7198f3c73295db945bfec34e0
Danel Martinez <martinezgdanel@hotmail.com>
2024-12-07 21:50:27 -0300

    feat: añadir AIStateMachine para gestionar estados de IA

---
+++ Assets/Zombie-Outbreak/Scripts/AI/AIStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIStateType { None, Idle, Alert, Patrol, Attack, Feeding, Pursuit, Dead }
+
+public abstract class AIStateMachine : MonoBehaviour
+{
+    // Private
+    private Dictionary<AIStateType, AIState> _states = new Dictionary<AIStateType, AIState>();
+
+    protected virtual void Start()
+    {
+        // Obtener todos los estados de este gameObject.
+        AIState[] states = GetComponents<AIState>();
+
+        // Recorremos todos los estados y lo añadimos al diccionario de estados.
+        foreach (AIState state in states)
+        {
+            if (state != null && !_states.ContainsKey(state.GetStateType()))
+            {
+                // Añadimos este estado al diccionario de estado.
+                _states[state.GetStateType()] = state;
+            }
+        }
+    }
+}
